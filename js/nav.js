065237e08eed5ef41072ac9541fadb3e66e2e814
yojuyoon <60738400+yojuyoon@users.noreply.github.com>
2020-05-21 00:24:01 +0900

    add media query
    nav 미디어 쿼리 적용, CSS변경

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector('.navbar__toggleBtn'),
+    menu = document.querySelector('.navbar__menu'),
+    icons = document.querySelector('.navbar__icons');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+});
